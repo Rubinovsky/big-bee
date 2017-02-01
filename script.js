cdb757f8fc25b79504eb8a5e338948c8b85896e9
Oles Rubinovsky <oles.rubinovscky@gmail.com>
2017-02-02 01:19:14 +0200

    Працював над модальним вікном покупки. Створив 2-гий список товарів.

--- script.js
+++ script.js
@@ -21,5 +21,23 @@ $(document).ready(function(){
 	    	text.slideDown("normal");
 	    	$(this).fadeOut(0);
 	    })
+	    $('.order').click(function(){
+	    	var par = $(this).parent();
+	    	var h_type = par.find("h3").text();
+	    	$(".honey_type").val(h_type);
+	    })
+
+	    $("#honey_form").submit(function() { //устанавливаем событие отправки для формы с id=form
+	            var form_data = $("#honey_form").serialize(); //собераем все данные из формы
+	            $.ajax({
+		            type: "POST", //Метод отправки
+		            url: "send.php", //путь до php фаила отправителя
+		            data: form_data
+	            });
+	            $("#honey_form input:text").val('');
+	        	$("#honey_form").modal('hide');
+	        	$("#small_modal").modal('show');
+	        	return false
+	    });
 
 	});    
